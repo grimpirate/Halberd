d09dc256b416f40dcecbf1750311be6afe5c08ac
grimpirate <grimpirate_jrs@yahoo.com>
2024-08-22 13:59:14 -0400

    es locale support

---
+++ src/Language/es/Halberd.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // 2FA
+    'title2FA'    => 'Verificación de Doble Factor',
+    'confirmCode' => 'Ingrese el código de 6 dígitos de su aplicación.',
+    'needVerification'  => 'Escanea el código QR para activar su cuenta.',
+
+    // Register
+    'googleApp'  => 'Escanea este código QR utilizando una aplicación de verificación de doble factor (<a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=es" target="_blank">Android</a>/<a href="https://apps.apple.com/es/app/google-authenticator/id388497605" target="_blank">iOS</a>) y ingrese una contraseña única (OTP) para activar su cuenta.',
+    'problems'  => '¿No puede escanear? Ingrese la clave de configuración manualmente <strong>{placeholder}</strong> en su aplicación.',
+];
